ec46628dc6ef109af64ea113ccb7836d2487c690
Chrisleerbeer <43794@hoornbeeck.nl>
2022-11-15 13:33:22 +0100

    nieuw laravel project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>READ</title>
+    <link href="{{ asset('css/master.css') }}" rel="stylesheet" type="text/css" >
+</head>
+<body>
+    <div class="hero">
+        <div class="hero-title">
+            <h1 class="title-home">THE<br><span class="title-focus"> NEWEST</span><br> BOOKS<br> FOR YOU</h1>
+        </div>
+        <div class="hero-books">
+            <div class="hero-books-img">
+                <img src="{{ asset('images/silmarillion.jpg') }}">
+            </div>    
+        </div>    
+    </div>
+    <div class="shelve">
+
+    </div>
+</body>
+</html>
